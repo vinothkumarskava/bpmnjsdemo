1937324b876d2471e81419f9de46622031be7b5f
vinoth <vinothkumar@skava.com>
2020-06-24 14:18:57 +0530

    Khurram feedback changes added to have a working model

--- app/app.js
+++ app/app.js
@@ -6,261 +6,178 @@ import ContextPadProvider from 'bpmn-js/lib/features/context-pad/ContextPadProvi
 import BpmnModeler from 'bpmn-js/lib/Modeler';
 import propertiesPanelModule from 'bpmn-js-properties-panel';
 import propertiesProviderModule from 'bpmn-js-properties-panel/lib/provider/bpmn';
-//import propertiesProviderModule from 'bpmn-js-properties-panel/lib/provider/camunda';
-import camundaModdleDescriptor from 'camunda-bpmn-moddle/resources/camunda.json';
-import { getBusinessObject } from 'bpmn-js/lib/util/ModelUtil';
-
+import BpmnPropertiesProvider from 'bpmn-js-properties-panel/lib/provider/bpmn/BpmnPropertiesProvider';
 import diagramXML from '../resources/diagram.bpmn';
-
 import customModule from './custom';
-
 import custeleExtension from '../resources/custelem';
 
-const HIGH_PRIORITY = 1500;
-
-const containerEl = document.getElementById('container'),
-      qualityAssuranceEl = document.getElementById('quality-assurance'),
-      suitabilityScoreEl = document.getElementById('suitability-score'),
-      lastCheckedEl = document.getElementById('last-checked'),
-      okayEl = document.getElementById('okay'),
-      formEl = document.getElementById('form'),
-      warningEl = document.getElementById('warning');
-
-var _getPaletteEntries = PaletteProvider.prototype.getPaletteEntries;
-PaletteProvider.prototype.getPaletteEntries = function(element) {
-    var entries = _getPaletteEntries.apply(this);
-    delete entries['create.intermediate-event'];
-    delete entries['create.task'];
-    delete entries['create.data-object'];
-    delete entries['create.participant-expanded'];
-    delete entries['create.subprocess-expanded'];
-    delete entries['create.data-store'];
-    delete entries['space-tool'];
-    delete entries['global-connect-tool'];
-    return entries; 
-};
-
-var _getContextPadEntries = ContextPadProvider.prototype.getContextPadEntries;
-ContextPadProvider.prototype.getContextPadEntries = function(element) {
-    var entries = _getContextPadEntries.apply(this, [element]);
-    delete entries['append.append-task'];
-    delete entries['append.intermediate-event'];
-    delete entries['append.text-annotation'];
-    delete entries['replace'];
-    return entries; 
-};
-      
-var container = $('#js-drop-zone');
-// hide quality assurance if user clicks outside
-/*window.addEventListener('click', (event) => {
-  const { target } = event;
-
-  if (target === qualityAssuranceEl || qualityAssuranceEl.contains(target)) {
-    return;
-  }
-
-  qualityAssuranceEl.classList.add('hidden');
-});*/
-
-// create modeler
-const bpmnModeler = new BpmnModeler({
-  container: '#js-canvas',
-  propertiesPanel: {
-    parent: '#js-properties-panel'
-  },
-  additionalModules: [
-    customModule,
-    propertiesPanelModule,
-    propertiesProviderModule
-  ],
-  moddleExtensions: {
-    custele: custeleExtension
-  },
-  keyboard: { bindTo: document }
-});
-
-
-function createNewDiagram() {
-  openDiagram(diagramXML);
-}
-
-function openDiagram(diagramXML) {
-// import XML
-bpmnModeler.importXML(diagramXML, (err) => {
-  if (err) {
-    container
-      .removeClass('with-diagram')
-      .addClass('with-error');
-
-    container.find('.error pre').text(err.message);
-
-    console.error(err);
-  } else {
-    container
-      .removeClass('with-error')
-      .addClass('with-diagram');
-  }
-
-  /*const moddle = bpmnModeler.get('moddle'),
-        modeling = bpmnModeler.get('modeling');
-
-  let analysisDetails,
-      businessObject,
-      element,
-      suitabilityScore;
-
-  // validate suitability score
-  function validate() {
-    const { value } = suitabilityScoreEl;
-
-    if (isNaN(value)) {
-      warningEl.classList.remove('hidden');
-      okayEl.disabled = true;
-    } else {
-      warningEl.classList.add('hidden');
-      okayEl.disabled = false;
-    }
-  }
-
-  // open quality assurance if user right clicks on element
-  bpmnModeler.on('element.contextmenu', HIGH_PRIORITY, (event) => {
-    event.originalEvent.preventDefault();
-    event.originalEvent.stopPropagation();
-
-    qualityAssuranceEl.classList.remove('hidden');
-
-    ({ element } = event);
-  
-    // ignore root element
-    if (!element.parent) {
-      return;
-    }
-
-    businessObject = getBusinessObject(element);
-  
-    let { suitable } = businessObject;
-    
-    suitabilityScoreEl.value = suitable ? suitable : '';
-
-    suitabilityScoreEl.focus();
-
-    analysisDetails = getExtensionElement(businessObject, 'qa:AnalysisDetails');
-
-    lastCheckedEl.textContent = analysisDetails ? analysisDetails.lastChecked : '-';
-
-    validate();
-  });
-
-  // set suitability core and last checked if user submits
-  formEl.addEventListener('submit', (event) => {
-    event.preventDefault();
-    event.stopPropagation();
-
-    suitabilityScore = Number(suitabilityScoreEl.value);
-
-    if (isNaN(suitabilityScore)) {
-      return;
-    }
-
-    const extensionElements = businessObject.extensionElements || moddle.create('bpmn:ExtensionElements');
-
-    if (!analysisDetails) {
-      analysisDetails = moddle.create('qa:AnalysisDetails');
-
-      extensionElements.get('values').push(analysisDetails);
-    }
-
-    analysisDetails.lastChecked = new Date().toISOString();
-
-    modeling.updateProperties(element, {
-      extensionElements,
-      suitable: suitabilityScore
+function getElementsConfigurations() {
+  return new Promise((resolve, reject) => {
+    $.ajax({
+      headers: {
+        "Accept": "application/vnd.github.VERSION.raw",
+        "Authorization": "token c15d20dd0b7b7f0326b15316757dbdf53a35631d"
+      },
+      url: "https://api.github.com/repos/vinothkumarskava/bpmnjsdemo/contents/resources/PriceRuleConfiguration.json",
+      success: function(response) {
+        if(response.length > 0) {
+          resolve(JSON.parse(response));
+        } else {
+          reject(response);
+        }
+      },
+      error: function(error) {
+        reject(error);
+      }
     });
-
-    qualityAssuranceEl.classList.add('hidden');
   });
-
-  // close quality assurance if user presses escape
-  formEl.addEventListener('keydown', (event) => {
-    if (event.key === 'Escape') {
-      qualityAssuranceEl.classList.add('hidden');
-    }
-  });
-
-  // validate suitability score if user inputs value
-  suitabilityScoreEl.addEventListener('input', validate);*/
-
-});
-
-}
-
-
-function saveSVG(done) {
-  bpmnModeler.saveSVG(done);
-}
-
-function saveDiagram(done) {
-
-  bpmnModeler.saveXML({ format: true }, function(err, xml) {
-    done(err, xml);
-  });
-}
-
-
-function registerFileDrop(container, callback) {
-
-  function handleFileSelect(e) {
-    e.stopPropagation();
-    e.preventDefault();
-
-    var files = e.dataTransfer.files;
-    var file = files[0];
-    var reader = new FileReader();
-    reader.onload = function(e) {
-      var xml = e.target.result;
-      callback(xml);
-    };
-
-    reader.readAsText(file);
-  }
-
-  function handleDragOver(e) {
-    e.stopPropagation();
-    e.preventDefault();
-
-    e.dataTransfer.dropEffect = 'copy'; // Explicitly show this is a copy.
-  }
-
-  container.get(0).addEventListener('dragover', handleDragOver, false);
-  container.get(0).addEventListener('drop', handleFileSelect, false);
-}
-
-
-// file drag / drop ///////////////////////
-
-// check file api availability
-if (!window.FileList || !window.FileReader) {
-  window.alert(
-    'Looks like you use an older browser that does not support drag and drop. ' +
-    'Try using Chrome, Firefox or the Internet Explorer > 10.');
-} else {
-  registerFileDrop(container, openDiagram);
-}
-
-function getExtensionElement(element, type) {
-  if (!element.extensionElements) {
-    return;
-  }
-
-  return element.extensionElements.values.filter((extensionElement) => {
-    return extensionElement.$instanceOf(type);
-  })[0];
 }
 
 // bootstrap diagram functions
-
 $(function() {
- 
+  getElementsConfigurations()
+  .then(configData => {
+    init(configData);
+  })
+  .catch(error => {
+    console.log("The config file cannot be loaded due to the following error: " + error);
+  })
+});
+
+function init(configData)
+{
+  var _getPaletteEntries = PaletteProvider.prototype.getPaletteEntries;
+  PaletteProvider.prototype.getPaletteEntries = function(element) {
+      var entries = _getPaletteEntries.apply(this);
+      delete entries['create.intermediate-event'];
+      delete entries['create.task'];
+      delete entries['create.data-object'];
+      delete entries['create.participant-expanded'];
+      delete entries['create.subprocess-expanded'];
+      delete entries['create.data-store'];
+      delete entries['space-tool'];
+      delete entries['global-connect-tool'];
+      return entries; 
+  };
+
+  var _getContextPadEntries = ContextPadProvider.prototype.getContextPadEntries;
+  ContextPadProvider.prototype.getContextPadEntries = function(element) {
+      var entries = _getContextPadEntries.apply(this, [element]);
+      delete entries['append.append-task'];
+      delete entries['append.intermediate-event'];
+      delete entries['append.text-annotation'];
+      delete entries['replace'];
+      return entries; 
+  };
+
+  BpmnPropertiesProvider.prototype.getElementsConfigurations = function() {
+    return configData;
+  };
+        
+  var container = $('#js-drop-zone');
+  // create modeler
+  const bpmnModeler = new BpmnModeler({
+    container: '#js-canvas',
+    propertiesPanel: {
+      parent: '#js-properties-panel'
+    },
+    additionalModules: [
+      customModule,
+      propertiesPanelModule,
+      propertiesProviderModule
+    ],
+    moddleExtensions: {
+      custele: custeleExtension
+    },
+    keyboard: { bindTo: document }
+  });
+
+
+  function createNewDiagram() {
+    openDiagram(diagramXML);
+  }
+
+  function openDiagram(diagramXML) {
+  // import XML
+  bpmnModeler.importXML(diagramXML, (err) => {
+    if (err) {
+      container
+        .removeClass('with-diagram')
+        .addClass('with-error');
+
+      container.find('.error pre').text(err.message);
+
+      console.error(err);
+    } else {
+      container
+        .removeClass('with-error')
+        .addClass('with-diagram');
+    }
+  });
+
+  }
+
+  function saveSVG(done) {
+    bpmnModeler.saveSVG(done);
+  }
+
+  function saveDiagram(done) {
+
+    bpmnModeler.saveXML({ format: true }, function(err, xml) {
+      done(err, xml);
+    });
+  }
+
+  function registerFileDrop(container, callback) {
+
+    function handleFileSelect(e) {
+      e.stopPropagation();
+      e.preventDefault();
+
+      var files = e.dataTransfer.files;
+      var file = files[0];
+      var reader = new FileReader();
+      reader.onload = function(e) {
+        var xml = e.target.result;
+        callback(xml);
+      };
+
+      reader.readAsText(file);
+    }
+
+    function handleDragOver(e) {
+      e.stopPropagation();
+      e.preventDefault();
+
+      e.dataTransfer.dropEffect = 'copy'; // Explicitly show this is a copy.
+    }
+
+    container.get(0).addEventListener('dragover', handleDragOver, false);
+    container.get(0).addEventListener('drop', handleFileSelect, false);
+  }
+
+
+  // file drag / drop ///////////////////////
+
+  // check file api availability
+  if (!window.FileList || !window.FileReader) {
+    window.alert(
+      'Looks like you use an older browser that does not support drag and drop. ' +
+      'Try using Chrome, Firefox or the Internet Explorer > 10.');
+  } else {
+    registerFileDrop(container, openDiagram);
+  }
+
+  function getExtensionElement(element, type) {
+    if (!element.extensionElements) {
+      return;
+    }
+
+    return element.extensionElements.values.filter((extensionElement) => {
+      return extensionElement.$instanceOf(type);
+    })[0];
+  }
+
   $('#js-create-diagram').click(function(e) {
     e.stopPropagation();
     e.preventDefault();
@@ -303,7 +220,7 @@ $(function() {
   }, 500);
 
   bpmnModeler.on('commandStack.changed', exportArtifacts);
-});
+}
 
 // helpers //////////////////////
 
